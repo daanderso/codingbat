f86164f73e33d52b5593b024e8c4bd9099d65ca3
Derek Anderson <daanderso@gmail.com>
2017-12-07 22:01:40 -0500

    new exercise

---
+++ src/string1/FirstHalf.java
@@ -0,0 +1,20 @@
+package string1;
+
+/*
+ *  Given a string of even length, return the first half.
+ *  So the string "WooHoo" yields "Woo".
+ *  
+ * firstHalf("WooHoo") -> "Woo"
+ * firstHalf("HelloThere") -> "Hello"
+ * firstHalf("abcdef") -> "abc"
+ */
+
+public class FirstHalf {
+
+	public String firstHalf(String str) {
+		int firstHalfRear = str.length() / 2;
+
+		return str.substring(0, firstHalfRear);
+
+	}
+}
