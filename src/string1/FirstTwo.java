bd3ac04a504b88652c05e907f06c4e69eacbea6c
Derek Anderson <daanderso@gmail.com>
2017-12-07 21:54:13 -0500

    new exercise

---
+++ src/string1/FirstTwo.java
@@ -0,0 +1,23 @@
+package string1;
+
+/* Given a string, return the string made of its first two chars, 
+ * so the String "Hello" yields "He". If the string is shorter than length 2, 
+ * return whatever there is, so "X" yields "X", and the empty string "" yields 
+ * the empty string "". Note that str.length() returns the length of a string.
+ * 
+ * firstTwo("Hello") -> "He"
+ * firstTwo("abcdefg") -> "ab"
+ * firstTwo("ab") -> "ab"
+ */
+
+public class FirstTwo {
+
+	public String firstTwo(String str) {
+		if (str.length() < 2) {
+			return str;
+		}
+
+		String firstTwo = str.substring(0, 2);
+		return firstTwo;
+	}
+}
