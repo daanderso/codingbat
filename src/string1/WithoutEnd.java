94e4de85070e49b2680a9d5d8c3a52c8200367d4
Derek Anderson <daanderso@gmail.com>
2017-12-11 21:44:38 -0500

    new exercise

--- src/string1/WithoutEnd.java
+++ src/string1/WithoutEnd.java
@@ -1,5 +1,20 @@
 package string1;
+/*
+ * 
+ * Given a string, return a version without the first and last char, 
+ * so "Hello" yields "ell". The string length will be at least 2.
+
+ * withoutEnd("Hello") -> "ell"
+ * withoutEnd("java") -> "av"
+ * withoutEnd("coding") -> "odin"
+ */
 
 public class WithoutEnd {
 
+	public String withoutEnd(String str) {
+
+		int strLength = str.length() - 1;
+
+		return str.substring(1, strLength);
+	}
 }
